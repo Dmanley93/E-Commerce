ddc03f966fbe0c43cbcff8962114c25b844cb920
daniel manley II <dmen1002@msn.com>
2021-05-03 17:20:10 -0500

    initial commit

---
+++ routes/api/category-routes.js
@@ -0,0 +1,90 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  try {
+    const allCategoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(allCategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  try {
+    const allCategoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!allCategoryData) {
+      res.status(404).json({ message: 'No Category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(allCategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+  try {
+    const allCategoryData = await Category.create({
+      product_id: req.body.product_id,
+    });
+    res.status(200).json(allCategoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+  try {
+    const allCategoryData = await Category.update({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!allCategoryData) {
+      res.status(404).json({ message: 'No Category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(allCategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+  try {
+    const allCategoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!allCategoryData) {
+      res.status(404).json({ message: 'No Category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(allCategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
